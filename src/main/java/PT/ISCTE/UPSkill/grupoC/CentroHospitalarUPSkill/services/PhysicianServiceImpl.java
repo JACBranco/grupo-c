2eb1b672008700a3bda182eda1e56ad659bd144f
JACBranco <joao.brn.89@gmail.com>
2022-09-21 23:57:48 +0100

    Criação do Projeto

--- src/main/java/PT/ISCTE/UPSkill/grupoC/CentroHospitalarUPSkill/services/PhysicianServiceImpl.java
+++ src/main/java/PT/ISCTE/UPSkill/grupoC/CentroHospitalarUPSkill/services/PhysicianServiceImpl.java
@@ -24,10 +24,13 @@ public class PhysicianServiceImpl implements PhysicianService {
     }
 
     private void initPhysicians() {
-        List<Physician> physicians = new ArrayList<>();
-        physicians.add(new Physician("Joao Bugalho","jbugalho","adoromedicina", LocalDate.of(1985, 6,22),"Rua das Bolachas 24","joao.bugalho@gmail.com","912345678","Lumiar","12345","Cardiologia"));
-        physicians.add(new Physician("Rita Cristina Silva","rcsilva","1mais1iguala2", LocalDate.of(1992, 3,12),"Avenida Fontes Pereira de Melo 2","rita.tina@hotmail.com","912222678","Lisboa","32145","Pediatria"));
-        physicians.add(new Physician("Crespo Luis Chuva","clchuva","umdiadecadavez123", LocalDate.of(1968, 12,2),"Rua Augusto Gil","Crespo.Chuva@hotmail.com","912355618","Porto","12435","Clinica Geral"));
 
+        List<Physician> physicians = new ArrayList<>();
+
+        physicians.add(new Physician("Joao Bugalho", "jbugalho", "adoromedicina", LocalDate.of(1985, 6, 22), "Rua das Bolachas 24", "joao.bugalho@gmail.com", "912345678", "Lumiar", "12345", "Cardiologia"));
+        physicians.add(new Physician("Rita Cristina Silva", "rcsilva", "1mais1iguala2", LocalDate.of(1992, 3, 12), "Avenida Fontes Pereira de Melo 2", "rita.tina@hotmail.com", "912222678", "Lisboa", "32145", "Pediatria"));
+        physicians.add(new Physician("Crespo Luis Chuva", "clchuva", "umdiadecadavez123", LocalDate.of(1968, 12, 2), "Rua Augusto Gil", "Crespo.Chuva@hotmail.com", "912355618", "Porto", "12435", "Clinica Geral"));
+
+        physicianRepository.saveAll(physicians);
     }
 }

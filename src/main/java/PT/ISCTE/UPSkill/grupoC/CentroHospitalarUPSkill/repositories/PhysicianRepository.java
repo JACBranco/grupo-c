aa9b267e1c837d41fa94817cb45042af3a5a33b6
JACBranco <joao.brn.89@gmail.com>
2022-09-22 00:04:28 +0100

    Criação do Projeto

--- src/main/java/PT/ISCTE/UPSkill/grupoC/CentroHospitalarUPSkill/repositories/PhysicianRepository.java
+++ src/main/java/PT/ISCTE/UPSkill/grupoC/CentroHospitalarUPSkill/repositories/PhysicianRepository.java
@@ -4,6 +4,11 @@ import PT.ISCTE.UPSkill.grupoC.CentroHospitalarUPSkill.models.Physician;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhysicianRepository extends JpaRepository<Physician, String> {
+
+    List<Physician> findPhysiciansByEspecialidade(String especialidade);
+
 }
